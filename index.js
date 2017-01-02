00c44611a9cb2ea92af141f717b302af14e5ddcc
maikel112233 <maikel112233@gmail.com>
2017-01-02 14:22:51 +0100

    uno

---
+++ index.js
@@ -0,0 +1,53 @@
+'use strict'
+
+const express = require('express')
+const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const Product = require('./models/product')
+const app= express()
+const port= process.env.PORT || 3001
+app.use(bodyParser.urlencoded({
+	extended: false
+}))
+app.use(bodyParser.json())
+
+app.get('/api/product',(req,res) => {
+	res.send(200,{products: []})
+}
+)
+app.get('/api/product/:productId',(req,res) => {
+	    
+})
+
+app.post('/api/product',(req,res) =>{ 
+	console.log('post /api/product')
+	console.log('req.boy')
+	let product = new Product()
+	product.name= req.body.name
+	product.picture = req.body.picture
+	product.price = req.body.price
+	product.description = req.body.description
+	
+	product.save((err,productStored)=>{
+	if (err) res.status(500).send({message: `error al salvar en la base de datos ${description}`})
+
+	res.status(200).send({product: productStored})
+})
+})
+
+app.put('/api/product/:productId',(req,res)=>{})
+
+app.delete('/api/product/:productdId',(req,res)=>{})
+
+
+mongoose.connect('mongodb://localhost:27017/shop',(err,res)=>{
+	if(err){
+		return console.log('error al concectar en la base de datos:')
+	}
+	 console.log('Conexión a labase de tatos establecida')
+
+ 
+app.listen(port, ()=>{
+	console.log(`express en localhost:${port}`)
+})
+})
